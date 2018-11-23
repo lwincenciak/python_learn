fd0f6e512e26e6714b82a08342c438311611078a
Leszek Wincenciak <leszekflickr@gmail.com>
2018-11-23 22:21:18 +0100

    if and while

--- ifs.py
+++ ifs.py
@@ -1,11 +1,39 @@
-is_male = True
-name = input('What is your name? ')
-if name[-1] == 'a':
-    print('You are a woman')
-else:
-    print('You are a man')
+import os
 
-# if is_male:
-#     print('You are a male')
-# else:
-#     print('You are a woman')
+is_male = False
+is_tall = False
+name = input('Twoje imie: ')
+height = input('Twoj wzrost w cm: ')
+while not(height.isdigit()):
+    print('Niepoprawna wartosc, podaj jeszcze raz')
+    height = input('Twoj wzrost w cm: ')
+
+os.system('cls')
+
+if name[-1] == 'a':
+    is_male = False
+else:
+    is_male = True
+
+if is_male and float(height) > 175:
+    is_tall = True
+elif not(is_male) and float(height) > 165:
+    is_tall = True
+else:
+    is_tall = False
+
+'''
+if is_male or is_tall:
+    print('You are male or tall')
+else:
+    print('You are not a male or not tall or both')
+'''
+
+if is_male and is_tall:
+    print('Jestes wysokim mezczyzna')
+elif is_male and not(is_tall):
+    print('Jestes niskim mezczyzna')
+elif not(is_male) and (is_tall):
+    print('Jestes wysoka kobieta')
+else:
+    print('Jestes niska kobieta')
