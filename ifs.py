e968d2a5ede5bb53f5bbf6e37132380b4a3e6934
Leszek Wincenciak <leszekflickr@gmail.com>
2018-11-23 21:25:34 +0100

    if statements

---
+++ ifs.py
@@ -0,0 +1,11 @@
+is_male = True
+name = input('What is your name? ')
+if name[-1] == 'a':
+    print('You are a woman')
+else:
+    print('You are a man')
+
+# if is_male:
+#     print('You are a male')
+# else:
+#     print('You are a woman')
--- kwadratowe.py
+++ kwadratowe.py
@@ -1,3 +1,5 @@
+from math import *
+
 a = input("a = ")
 b = input("b = ")
 c = input("c = ")
@@ -5,14 +7,17 @@ delta = float(b) * float(b) - 4 * float(a) * float(c)
 print("\nRownanie kwadratowe: " + a + "x^2 + " + b + "x + " + c + " = 0")
 print("Delta =" + str(delta))
 
-
-'''
-roznica = float(num1) - float(num2)
-iloczyn = float(num1) * float(num2)
-iloraz = float(num1) / float(num2)
-
-print("\n" + num1 + " + " + num2 + " =", str(suma))
-print("\n" + num1 + " - " + num2 + " =", str(roznica))
-print("\n" + num1 + " * " + num2 + " =", str(iloczyn))
-print("\n" + num1 + " / " + num2 + " =", str(iloraz))
-'''
+if delta < 0:
+    print('Rownanie nie ma pierwiastkow.')
+else:
+    if delta == 0:
+        print('Rownanie ma jeden pierwiastek.')
+        x = -float(b) / (2 * float(a))
+        print('x1 = x2 = ' + str(x))
+    else:
+        delt = sqrt(float(delta))
+        print('Rownanie ma dwa pierwiastki')
+        x_1 = (-float(b) - float(delt)) / (2 * float(a))
+        x_2 = (-float(b) + float(delt)) / (2 * float(a))
+        print('x1 = ' + str(x_1))
+        print('x2 = ' + str(x_2))
