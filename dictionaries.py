76df088158a5e600469678501eb2c440b04e1210
Leszek Wincenciak <leszekflickr@gmail.com>
2018-11-26 22:36:20 +0100

    dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,39 @@
+'''
+Jan -> January
+Feb -> February
+key -> value
+'''
+
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+monthConv = {
+    1: "January",
+    2: "February",
+    3: "March",
+    4: "April",
+    5: "May",
+    6: "June",
+    7: "July",
+    8: "August",
+    9: "September",
+    10: "October",
+    11: "November",
+    12: "December"
+}
+
+print(monthConversions.get("Dec", "Not a valid key"))
+print(monthConversions.get("De", "Not a valid key"))
+print(monthConv.get(11, "Not a valid key"))
