dae5061b2cbf14e493f2848e0f2a2e911d3eafac
Leszek Wincenciak <leszekflickr@gmail.com>
2018-11-28 13:21:53 +0100

    28/11/2018

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,9 +1,16 @@
 # This is a simple simulation of throwing a dice a number of times
 # The average and std. dev. are calculated and stored in a list
 # Everything is then exported to a file 'dice_mc.csv'
-import useful_tools as ut
 import numpy as np
 import sys
+import random
+import matplotlib.pyplot as plt
+import csv
+from matplotlib.ticker import AutoMinorLocator, MultipleLocator, FuncFormatter
+
+
+def roll_dice(number):
+    return random.randint(1, number)
 
 
 def my_print(text):
@@ -13,29 +20,39 @@ def my_print(text):
 
 def kropki(num):
     my_print(".")
-    rest = int(num) % 50
-    if rest == 0:
-        print(" " + str(num))
+    rest1 = int(num) % 50
+    rest2 = int(num) % 10
+    if rest2 == 0:
+        my_print("|")
+    if rest1 == 0:
+        print(" : " + str(num))
 
 
-dice_throws = 1200
-iterations = 200
+def minor_tick(x, pos):
+    if not x % 1.0:
+        return ""
+    return "%.2f" % x
 
-dice = []
+
+dice_throws = 2400
+iterations = 1000
+
 dice_file = open("dice_mc.csv", "w")
-dice_file.write("Average;Std\n")
+# dice_file.write("Average;Std\n")
 dice_file = open("dice_mc.csv", "a")
 
 for z in range(iterations):
+    dice = []  # this clears the list of dice results for every z!
     for i in range(dice_throws):
         # print("Rolling dice " + str(i))
         # print(ut.roll_dice(6))
-        dice.append(ut.roll_dice(6))
+        dice.append(roll_dice(6))
         # print(dice)
     dice_av = np.average(dice)
     dice_std = np.std(dice)
     # print("Average: " + str(dice_av))
     # print("Stdev. : " + str(dice_std))
+    dice_file.write(str(z + 1) + ";")
     dice_file.write(str(dice_av) + ";")
     dice_file.write(str(dice_std) + "\n")
     kropki(z + 1)
@@ -48,3 +65,43 @@ print("\nOutput written to file: dice_mc.csv")
 # dice_file = open("dice_mc.txt", "r")
 # print(dice_file.read())
 # dice_file.close()
+
+
+# I would like to draw the results by reading the FileExistsError
+
+x = []
+y = []
+
+with open('dice_mc.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=';')
+    for row in plots:
+        x.append(int(row[0]))
+        y.append(float(row[1]))
+
+# fig = plt.figure(figsize=(12, 5))
+# fig = plt.figure(figsize=(10, 1))
+# ax = fig.add_subplot(1, 1, 1, aspect=1)
+
+# ax.xaxis.set_major_locator(MultipleLocator(100))
+# ax.xaxis.set_minor_locator(AutoMinorLocator(10))
+# ax.yaxis.set_major_locator(MultipleLocator(1))
+# ax.yaxis.set_minor_locator(AutoMinorLocator(1))
+# ax.xaxis.set_minor_formatter(FuncFormatter(minor_tick))
+
+# ax.set_xlim(0, iterations)
+# ax.set_ylim(0, 6)
+
+# ax.tick_params(which='major', width=1.0)
+# ax.tick_params(which='major', length=10)
+# ax.tick_params(which='minor', width=1.0, labelsize=10)
+# ax.tick_params(which='minor', length=5, labelsize=10, labelcolor='0.25')
+
+# ax.grid(linestyle="--", linewidth=0.5, color='.25', zorder=-10)
+
+plt.plot(x, y, label='Average score')
+plt.xlabel('Iteration')
+plt.ylabel('Average score')
+plt.title('Simulation of ' + str(dice_throws) +
+          ' throws of a dice\nRepeated ' + str(iterations) + ' times')
+plt.legend()
+plt.show()
