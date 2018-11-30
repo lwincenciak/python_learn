a584c57285dff8fbc1a0e009e04f2e0140835378
Leszek Wincenciak <leszekflickr@gmail.com>
2018-11-30 20:13:09 +0100

    30/11/2018

--- mul_choice.py
+++ mul_choice.py
@@ -2,14 +2,14 @@ from Question import Question
 import random
 
 question_prompts = [
-    "What is the name of a lead singer of Queen?\n(a) Freddie\n(b) Brian\n(c) Roger\n\nYour answer: ",
-    "Who plays guitar in Queen?\n(a) Freddie\n(b) John\n(c) Brian\n\nYour answer: ",
-    "Who plays bass in Queen?\n(a) Roger\n(b) John\n(c) Brian\n\nYour answer: ",
+    "What is the name of a lead singer of Queen?\n(a) Freddie Mercury\n(b) Brian May\n(c) Roger Taylor\n\nYour answer: ",
+    "Who plays guitar in Queen?\n(a) Freddie Mercury\n(b) John Deacon\n(c) Brian May\n\nYour answer: ",
+    "Who plays bass in Queen?\n(a) Roger Taylor\n(b) John Deacon\n(c) Brian May\n\nYour answer: ",
     "When was the Bohemian Rhapsody released?\n(a) 1974\n(b) 1975\n(c) 1976\n\nYour answer: ",
     "Which album was illustrated with a giant robot?\n(a) News of the world\n(b) Innuendo\n(c) The Game\n\nYour answer: ",
     "Who was Queen engineer in 1975?\n(a) Queen themselves\n(b) Reinhold Mack\n(c) Roy Thomas Baker\n\nYour answer: ",
     "\"(...) and I advertise a soul for sale or rent...\" is lyrics from:\n(a) Save me\n(b) It's a hard life\n(c) Love of my life\n\nYour answer: ",
-    "He was the quiet one and turned to be our secret weapon. Who was it?\n(a) Roger\n(b) Brian\n(c) John\n\nYour answer: ",
+    "He was the quiet one and turned to be our secret weapon. Who was it?\n(a) Roger Taylor\n(b) Brian May\n(c) John Deacon\n\nYour answer: ",
     "Freddie was born in:\n(a) London, UK\n(b) Zanzibar\n(c) Bombay, India\n\nYour answer: ",
     "Brian May built his own guitar using parts of:\n(a) a fireplace\n(b) a motorcycle springs\n(c) both\n\nYour answer: ",
 ]
