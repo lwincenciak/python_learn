204bbbe955f77bf0ff2a37e467389831cdd06e15
Leszek Wincenciak <leszekflickr@gmail.com>
2018-12-17 13:30:26 +0100

    17/12/2018
    17/12/2018

--- poisson-goals.py
+++ poisson-goals.py
@@ -9,7 +9,7 @@ def silnia(n):
         return 1
 
 
-ts_length = 90
+match_length = 90
 b = 46 / 16  # Barcelona currently
 
 # p_values = [0]  # empty list
@@ -19,14 +19,14 @@ colors = ['#0055DD', '#009933', '#FF6622', '#FF1122', '#880000']
 
 def poisson(g):
     p_values = []
-    for t in range(ts_length + 1):
+    for t in range(match_length + 1):
         p = (np.exp(-(b / 90) * t) * ((b / 90) * t) ** g) / (silnia(g))
         p_values.append(p)
     return p_values
 
 
 fig = plt.figure(figsize=(10, 8))
-plt.xlim(0, ts_length)
+plt.xlim(0, match_length)
 # plt.ylim(3.35, 3.65)
 
 plt.rcParams.update({'font.size': 14})
@@ -40,7 +40,7 @@ for g in goals_list:
 plt.grid(True, 'major', 'both', ls='--', lw=.5, c='k', alpha=.3)
 plt.xlabel('Time [t]')
 plt.ylabel('')
-plt.title('Probability of scoring by Barcelona\n given number of goals in a game')
+plt.title('Probability of scoring given number of goals\n by Barcelona during a game')
 plt.legend()
 plt.show()
 fig.savefig("poisson.pdf")
