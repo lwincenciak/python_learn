77324b0144b5e3be19163cdff37d6c29640b1692
Leszek Wincenciak <leszekflickr@gmail.com>
2019-04-16 22:06:52 +0200

    16/04/2019

--- json/jsons.py
+++ json/jsons.py
@@ -1,43 +1,15 @@
-import requests
 import json
 
-# Now we have to request our JSON data through
-# the API package
-res = requests.get("https://jsonplaceholder.typicode.com / todos")
-var = json.loads(res.text)
+with open('users.json') as f:
+    d = json.load(f)
 
-# To view your Json data, type var and hit enter
-var
+for record in d:
+    print('Street: ' + record['address']['street'])
+    print('Geo_lat: ' + record['address']['geo']['lat'])
+    print('Geo_lng: ' + record['address']['geo']['lng'])
 
-# Now our Goal is to find the User who have
-# maximum completed their task !!
-# i.e we would count the True value of a
-# User in completed key.
-# {
-# "userId": 1,
-# "id": 1,
-# "title": "Hey",
-# "completed": false, # we will count
-# this for a user.
-# }
+with open('distros.json', 'r') as f:
+    distros_dict = json.load(f)
 
-# Note that there are multiple users with
-# unique id, and their task have respective
-# Boolean Values.
-
-
-def find(todo):
-    check = todo["completed"]
-    max_var = todo["userId"] in users
-    return check and max_var
-
-
-# To find the values.
-
-Value = list(filter(find, todos))
-
-# To write these value to your disk
-
-with open("sample.json", "w") as data:
-    Value = list(filter(keep, todos))
-    json.dump(Value, data, indent=4)
+for distro in distros_dict:
+    print(distro['Name'])
